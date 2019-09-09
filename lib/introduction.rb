10c80950714570183fb2edd89d8e7fa3fe0abb5a
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-09 20:28:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,11 @@ def introduction(name)
   puts "Hi, my name is Josh.\n"
   
 end
+
+def introduction_with_language(name, language)
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
+  
+end
+
+introduction_with_language("Dan", "Ember.js")
