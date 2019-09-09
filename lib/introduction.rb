9d9e8f69870fb811de1c865910bc3e7eab511bd2
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-09 20:28:35 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,13 @@
+def introduction(name)
+  
+  puts "Hi, my name is Josh.\n"
+  
+end
+
+def introduction_with_language(name, language)
+  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+  
+end
+
+introduction_with_language("Dan", "Ember.js")
